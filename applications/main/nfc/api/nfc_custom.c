90af06a87c68019b47472b32b31f1ca3f08f3867
bezdez <mimmo4545@gmail.com>
2025-01-30 21:17:44 +0300

    Добавлены новые NFC-функции

---
+++ applications/main/nfc/api/nfc_custom.c
@@ -0,0 +1,6 @@
+#include "nfc_custom.h"
+
+void nfc_custom_function(void) {
+    // Пример кода для работы с UID
+    printf("Custom NFC function triggered!\n");
+}
---
+++ applications/main/nfc/api/nfc_custom.h
@@ -0,0 +1,8 @@
+#ifndef NFC_CUSTOM_H
+#define NFC_CUSTOM_H
+
+#include <stdio.h>
+
+void nfc_custom_function(void);
+
+#endif // NFC_CUSTOM_H
